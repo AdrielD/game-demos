76e750322fbb3115c4f59741ca76343b187bcd54
adinelli <adinelli@corp.mediaresponse.com>
2016-02-24 18:12:13 -0300

    another commit

--- games/colors.js
+++ games/colors.js
@@ -1,20 +1,13 @@
 (function() {
-	console.log("hi!");
-
 	var canvas, ctx, MAX_WIDTH, MAX_HEIGHT;
 
 	window.addEventListener("DOMContentLoaded", function() {
-		loadGlobal();
-	});
-
-	function loadGlobal() {
 		canvas = document.getElementById("canvas");
 		ctx = canvas.getContext("2d");
 		MAX_WIDTH = canvas.width;
 		MAX_HEIGHT = canvas.height;
 
 		ctx.lineWidth = 125;
-		// ctx.strokeStyle = "#FFF";
 		ctx.strokeStyle = "rgba(1,1,1,1)";
 		ctx.lineJoin = ctx.lineCap = 'round';
 
@@ -31,12 +24,7 @@
 		canvas.addEventListener("mousemove", function(event) {
 			mousePaint(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
 		});
-	}
-
-	function clear() {
-		ctx.fillStyle = "#000";
-		ctx.fillRect(0, 0, MAX_WIDTH, MAX_HEIGHT);
-	}
+	});
 
 	function mousePaint(mouseX, mouseY) {
 		ctx.lineTo(mouseX, mouseY);
